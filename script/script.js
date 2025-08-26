dbb964f4c31d5616aa4bbf248506ed79e190d821
Awaisahmed13 <awaisahmedkhan2662@gmail.com>
2025-08-26 05:26:35 -0700

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,92 @@
+// let a = prompt("Enter a number: ");
+// let c = prompt("Enter a opration (+, -, *, /): ");
+// let b = prompt("Enter another number: ");
+
+// let result;
+// if (c === "+") {
+//   result = a + b;
+// } else if (c === "-") {
+//   result = a - b;
+// } else if (c === "*") {
+//   result = a * b;
+// } else if (c === "/") {
+//   result = a / b;
+// } else {
+//   result = "Invalid operation";
+// }
+// alert("The result is: " + result);
+// console.log("The result is: " + result);
+
+
+// let a = Number(prompt("Enter a number: "));
+// let c = prompt("Enter an operation (+, -, *, /): ");
+// let b = Number(prompt("Enter another number: "));
+
+// let result;
+// if (c === "+") {
+//     result = a + b;
+// } 
+// else if (c === "-") {
+//     result = a - b;
+// }
+// else if (c === "*") {
+//     result = a * b;
+// } 
+// else if (c === "/") {
+//     result = a / b;
+// } 
+// else {
+//     result = "Invalid operation";
+// }
+
+// alert("The result is: " + result);
+// console.log("The result is: " + result);
+ 
+
+// let obj = {
+//     1: "crazy",
+//     2: "Amazing",
+//     3: "Fire"
+// };
+
+// let obj2 = {
+//     1: "Engine",
+//     2: "Foods",
+//     3: "Garments"
+// };
+
+// let obj3 = {
+//     1: "Bros",
+//     2: "Limited",
+//     3: "Hub"
+// };
+
+// let random1 = Math.floor(Math.random() * 3)+ 1;
+// let random2 = Math.floor(Math.random() * 3)+ 1;
+// let random3 = Math.floor(Math.random() * 3)+ 1;
+
+// console.log(obj[random1] + " " + obj2[random2] + " " + obj3[random3]);
+
+
+// let number = Number(prompt("Enter a number: "));
+// let result = 1;
+
+// for (let i = 1; i <= number ; i++) {
+//     result *= i;
+// }
+// alert("The factorial of " + number + " is: " + result);
+// console.log("The factorial of " + number + " is: " + result)
+
+
+// let num = Number(prompt("Enter a number: "));
+
+// function factorialReduce(n) {
+//     if (n === 0 || n === 1) return 1;
+//     return Array.from({length: n}, (_, i) => i + 1)
+//                 .reduce((acc, curr) => acc * curr, 1);
+// }
+
+// let result = factorialReduce(num);
+
+// alert("Factorial of " + num + " is: " + result);
+// console.log("Factorial of " + num + " is: " + result);
